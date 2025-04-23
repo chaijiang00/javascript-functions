f21ac9eb5996bf4607e32260dc465f2582a6aa4b
chaijiang <chaijiangchaisan@gmail.com>
2025-04-23 17:36:37 +0700

    fix:ex1-6

--- ex-4.js
+++ ex-4.js
@@ -1,2 +1,23 @@
 // Start coding here
-let calculator = {};
+const add = (a,b)=>{ return a+b;};
+const subtract = (a,b)=>{ return a-b;};
+const multiply =(a,b)=>{return a*b;};
+const divide =(a,b)=>{return a/b;};
+
+let calculator = {
+    add : add,
+    subtract : subtract,
+    multiply : multiply,
+    divide : divide
+};
+
+console.log(calculator.add(10,5));
+console.log(calculator.subtract(10,5));
+console.log(calculator.multiply(10,5));
+console.log(calculator.divide(10,5));
+
+const addResult = calculator.add(10,20);
+console.log(addResult);
+
+const divideResult = calculator.divide(3000,10);
+console.log(divideResult);
